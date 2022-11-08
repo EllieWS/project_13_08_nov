61f320f8cc68222dc40e517ff4b701f8860e8dba
Ellie Sandelin <114465546+EllieWS@users.noreply.github.com>
2022-11-08 21:02:39 +1300

    JavaScript querySelector

---
+++ main.js
@@ -0,0 +1,47 @@
+//alert ("hello world")
+
+// console.log ("hello world")
+
+// var no = 1000;
+// console.log(no)
+
+// var name  = "mary";
+// console.log(name)
+
+// var no1 = 1000
+// var no2 = 2000
+// console.log(no1 + no2)
+
+// var no1 = 4000
+// console.log(no1 + no2)
+
+
+//camel casing, used for JavaScript
+// document.getElementById("header").style.color="#f33";
+// document.getElementById("header").style.border="2px solid"
+
+// document.getElementById("text").style.backgroundColor="#000";
+// document.getElementById("text").style.color="#fff"
+
+// //kebab-casing   get-element-by-id
+// //snake casing   get_element_by_id
+// //title casing   GetElementById
+
+// var myBtn = document.getElementById("btn")
+// myBtn.style.backgroundColor="#fff"
+// myBtn.style.width="200px"
+// myBtn.style.height="100px"
+// myBtn.style.fontSize=   "1em"
+// myBtn.style.borderRadius="20px"
+
+document.querySelector("h1").style.backgroundColor="#0f0"
+document.querySelector("#header").style.border="5px solid"
+document.querySelector(".heading").style.fontSize="5em"
+document.querySelector("[data-header]").style.fontFamily="Arial"
+
+document.querySelector("body").classList.add("homepage");
+document.querySelector("body").classList="body";
+
+document.querySelector("h1").classList.remove("heading")
+
+document.body.style.color="#0ec"
